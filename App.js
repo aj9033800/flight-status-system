a02071b503f3168066614a6ab9d58fad07e20daa
aj9033800 <aj9033800@gmail.com>
2024-07-29 12:19:30 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FlightStatus from './components/FlightStatus';
+
+function App() {
+    return (
+        <div className="App">
+            <FlightStatus />
+        </div>
+    );
+}
+
+export default App;
